b2f7ff56cd43fc7a7de3b4e91314e360cff8505e
Архипов Юрий Владимирович <ayv@altm.local>
2022-11-29 23:48:40 +0700

    decided 2 first a tasks

--- Homeworks_C#/Program.cs
+++ Homeworks_C#/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Домашняя работа 1
+Задача 1 (Напишите программу, которая на вход принимает 2 числа а на выходе выдает какое больше, а какое меньше)
+
+Console.WriteLine("Input a first number: ");
+ int  num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Input a second number: ");
+ int  num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ if(num1 > num2)
+ {
+  Console.WriteLine("largest number is " + num1 + ";" + " smallest number is " + num2);  
+ }
+ else
+{
+  Console.WriteLine("largest number is " + num2 + ";" + " smallest number is " + num1);  
+}
+Задача 2 (Напишите программу, которая на вход принимает 3 числа а на выходе выдает максимальное из этих чисел)
+*/
+Console.WriteLine("Input a first number: ");
+ int  num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Input a second number: ");
+ int  num2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Input a third number: ");
+ int  num3 = Convert.ToInt32(Console.ReadLine());
+
+ int max = num1;
+ 
+
+ if(num1 > num2)
+ {
+   max = num1;
+ }
+ else
+ {
+   max = num2;
+ }
+ if(max > num3)
+ {
+ Console.WriteLine("largest number is " + max);
+ }
+ else
+ {
+ Console.WriteLine("largest number is " + num3);
+ }
+ 
+ 
