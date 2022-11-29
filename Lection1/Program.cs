1a525d05865ce239cde7968c61e888085a8646f7
Архипов Юрий Владимирович <ayv@altm.local>
2022-11-29 22:10:14 +0700

    added Homeworks_C# and Lection1 folders

---
+++ Lection1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Домашняя работа
+ Console.WriteLine("Input a first number: ");
+ int  num1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Input a second number: ");
+ int  num2 = Convert.ToInt32(Console.ReadLine());
+
+ int quad = num2 * num2;
+ if(num1 == quad)
+ {
+  Console.WriteLine("Yes!");  
+ }
+ else
+{
+  Console.WriteLine("No!");  
+}
+Задача 2
+Console.Write("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(num <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
