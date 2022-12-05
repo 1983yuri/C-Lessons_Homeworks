b66be7c20714309a7e634ff6a1db7196a28f346a
Архипов Юрий Владимирович <ayv@altm.local>
2022-12-05 12:32:27 +0700

    the task about two friend and dog is checking with Metod

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -1,30 +1,46 @@
-﻿int count = 0;
-double distance = 10000;
-int FirstFriendSpeed = 1;
-int SecondFriendSpeed = 2;
-int DogSpeed = 5;
-int friend = 2;
-double time = 0;
-
+﻿int FriendsVsDog(double distance, int firstFriendSpeed, int secondFriendSpeed, int dogSpeed)
+{
+    int count = 0;
+    int friend = 2;
+    double time = 0;
 
 while(distance > 10)
 {
   if(friend == 1)
       {
-      time = distance / (FirstFriendSpeed + DogSpeed);
+      time = distance / (firstFriendSpeed + dogSpeed);
       friend = 2;
-      distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+      distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
       count++;
       }
   else
       {
-      time = distance / (SecondFriendSpeed + DogSpeed);
+      time = distance / (secondFriendSpeed + dogSpeed);
       friend = 1;
-      distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
-      count++;
-      }         
+      distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+      count++; 
+      }     
 }
-Console.Write(count);
+return count;
+}
+
+
+Console.WriteLine("Enter the distance in meters: ");
+double distance = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a first friend speed: ");
+int firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a second friend speed: ");
+int secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a dog speed: ");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+
+int friendvsdog = FriendsVsDog(distance, firstFriendSpeed, secondFriendSpeed, dogSpeed);
+
+
+Console.Write($"The dog will run from one friend to the second friend: {friendvsdog} times!");
 
     
 
