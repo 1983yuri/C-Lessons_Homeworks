c5f6016a5e9ac5add190b33cfbb1405a13b44113
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-07 22:11:14 +0700

    checking all tasks of 3 homework

--- Homeworks_C#/Program.cs
+++ Homeworks_C#/Program.cs
@@ -233,26 +233,73 @@ int num = Convert.ToInt32(Console.ReadLine());
 Pal(num);
 
 Вариант решения общий
-*/
+
 void Pal(int num)
 {
   int revers = 0;
   int temp = num;
   while(temp > 0)
   {
-    revers = revers * 10 + temp % 10;
-    temp = temp / 10;
+    revers = revers * 10 + temp % 10; // Формируем реверсное число по одному элементу
+    temp = temp / 10; // Отсекаем по одному элементу справа налево от изначального числа 
   }
   if(revers == num)
   {
-    Console.WriteLine("This number is a polindrom!");
+    Console.WriteLine($"This number -  {num} is a polindrom!");
   }
   else
   {
-    Console.WriteLine("This number is not a polindrom!");
+    Console.WriteLine($"This number -  {num} is not a polindrom!");
   }
 }
 Console.WriteLine("Input an integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Pal(num);
+
+Задача 2 (Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве)
+
+
+double Lenght(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+return Math.Round(Math.Sqrt (Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2); // Теорема пифагора для куба + округление значения до 2 знаков после запятой   
+}
+
+Console.WriteLine("Введите координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double lenght = Lenght(x1, y1, z1, x2, y2, z2);
+
+Console.WriteLine($"Расстояние между двумя точками равно: {lenght}");
+
+Задача 3 (Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N)
+*/
+
+void CubeToN(int num) 
+{
+int current = 1; 
+while(current <= num)
+{   
+    Console.Write(Math.Pow(current, 3) + " "); // Начинаем с 1 и возводим в степень 3 пока current <= num
+    current++;    
+}
+}
+
+Console.Write("Input an integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeToN(number);
