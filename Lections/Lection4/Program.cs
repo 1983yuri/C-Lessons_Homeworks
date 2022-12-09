2e3f11c746d90fd55280af56e6d7d922ac9b2007
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-09 22:18:34 +0700

    cheng Lection folder

--- Lections/Lection4/Program.cs
+++ Lections/Lection4/Program.cs
@@ -19,6 +19,45 @@ int result = NumOfDigits(number);
 
 Console.WriteLine($"Number of digits in {number} is {result}");
 
+/*
+Задача 1 с семинара , ЦИКЛ "for" (Напишите программу, которая выводит на экран сумму чисел от 1 до N)
+
+int SumN(int num)
+{    
+    int result = 0; // Перед циклом for, лучше сразу инициализировать переменную для записи результата
+    for(int count = 1; count <= num; count++)
+    {
+        result += count; // Тоже самое, что result = result + count
+    }
+    return result;
+}
+
+Console.WriteLine("Input integer number: ");
+int usernum = Convert.ToInt32(Console.ReadLine());
+
+int sumN = SumN(usernum);
+Console.WriteLine($"Sum of numbers from 1 to {usernum} is {sumN}");
+
+
+Задача 2 с семинара , ЦИКЛ "for" (Напишите программу, которая выводит на экран факториал чисел от 1 до N)
+
+
+int Fact(int num)
+{
+    int result = 1; // Начинаем с 1 т.к. умножение на 0 даст 0 и программа не будет работать, если пользователь введет 0, цикл не сработает и выдаст результат = 1
+    for(int count = 2; count <= num; count++)
+    {
+        result *= count; // Тоже самое, что result = result * count
+    }
+    return result;
+}
+
+Console.WriteLine("Input an integer number: ");
+int usernum = Convert.ToInt32(Console.ReadLine());
+
+int sumMult = Fact(usernum);
+Console.WriteLine($"The factorial of {usernum} is {sumMult}");
+
 
 Массивы(Метод заполнения массива случайными числами в заданном диапазоне)
 */
