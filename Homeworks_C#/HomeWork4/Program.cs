f7fb76473b64be4b65d0160cf20b2a6767a31493
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-10 12:10:42 +0700

    cheking first task from 4 homework

--- Homeworks_C#/HomeWork4/Program.cs
+++ Homeworks_C#/HomeWork4/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Домашняя работа 4 
+// Задача 1(Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B)
+
+double CustomMathPow(double num, int exp)
+{
+    double result = num;
+    for(int count = 1; count < exp; count++)
+    {
+         result *= num; 
+    }
+    return result;
+}
+
+Console.WriteLine("Input the number to be raised to the power: ");
+double power = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input a natural number, degree: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+double mathpow = CustomMathPow(power, degree);
+
+Console.WriteLine($"The number {power} to the power {degree} is {mathpow}");
+
