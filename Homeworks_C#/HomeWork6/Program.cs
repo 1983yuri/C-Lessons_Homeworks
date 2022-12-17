9bf106c154527a260841363873f9d7efda0fa3f6
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-17 21:44:43 +0700

    pre checking 2 task for 6 homework

--- Homeworks_C#/HomeWork6/Program.cs
+++ Homeworks_C#/HomeWork6/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Домашняя работа 6
 //Задача 1 ( Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь)
-
-int SunOfPosNum(int sizenum)
+/*
+int SumOfPosNum(int sizenum)
 {
    int count = 0;
    int numOfnum = 0;
@@ -21,5 +21,28 @@ int SunOfPosNum(int sizenum)
 
 Console.WriteLine("How many numbers will you input?");
 int sizenum = Convert.ToInt32(Console.ReadLine());
-int result = SunOfPosNum(sizenum);
-Console.WriteLine($"The number of positive {result}");
+int result = SumOfPosNum(sizenum);
+Console.WriteLine($"The number of positive is: {result}");
+
+
+// Задача 2 ( Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем)
+*/
+
+Console.WriteLine("Input the number b1 responsible for the location relative to the Y axis:");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input angular coefficient k1:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the number b2 responsible for the location relative to the Y axis:");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input angular coefficient k2:");
+int k2 = Convert.ToInt32(Console.ReadLine());
+    if (k1 == k2 && b1 != b2)
+        Console.WriteLine("These lines do not intersect!");
+    if (k1 == k2 && b1 == b2)
+        Console.WriteLine("These lines intersect at all points!");
+double x = 0;
+    x = (-b2 + b1) / (-k1 + k2);
+double y = 0;
+    y = k2 * x + b2;
+
+Console.WriteLine($"The intersection point has coordinates: x = {x} y = {y}");
