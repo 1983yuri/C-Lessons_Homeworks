7629abad80d5c91cfed5a3b2fde7d41b917fed19
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-21 11:06:02 +0700

    chekin 1 task of 7 homework

---
+++ Homeworks_C#/Homework7/Program.cs
@@ -0,0 +1,37 @@
+﻿// Домашняя работа 7
+// Задача 1 (Задайте двумерный массив размером m×n, заполненный случайными вещественными числами)
+
+double [,] CreateRandom2dArray() // Двумерный массив, сколько запятых столько измерений
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of colums: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+        double[,] array = new double[rows, columns];
+    
+    for(int i = 0; i < rows; i++) // Пробегаем по всем строкам
+        for(int j = 0; j < columns; j++) // Пробегаем по всем столбцам
+            array[i, j] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();      
+    
+    return array;
+}
+
+void Show2dArray(double[,] array) // Метод вывода 2х мерного массива
+{
+    for(int i = 0; i < array.GetLength(0); i++) // Ноль по строкам, 1 по столбцам
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+           Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] array = CreateRandom2dArray();
+Show2dArray(array);
