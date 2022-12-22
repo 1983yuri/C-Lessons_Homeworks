be544465b017425227ed843abf1a73b457717749
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-22 20:35:18 +0700

    chekin recursion Example015 folder

---
+++ Examples/Example015_ Recursion/Program.cs
@@ -0,0 +1,167 @@
+﻿// Рекурсия
+
+// Задача 1 собрать строку с числами от a до b, причем a <= b
+// Итеративный подход
+
+string NumbersFor(int a, int b)
+{
+ string result = String.Empty;
+ for (int i = a; i <= b; i++)
+ {
+ result += $"{i} ";
+ }
+ return result;
+}
+
+// Решение с помощью рекурсии
+
+string NumbersRec(int a, int b)
+{
+ if (a <= b) return $"{a} " + NumbersRec(a + 1, b);  // Рекурсивный вызов функции
+ else return String.Empty;                           // Условие выхода
+}
+Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
+// Задача 2 найти сумму чисел от 1 до N
+
+// Итеративный подход
+
+int SumFor(int n)
+{
+ int result = 0;
+ for (int i = 1; i <= n; i++) result += i;
+ return result;
+}
+
+// Рекурсия
+
+int SumRec(int n)
+{
+ if (n == 0) return 0;                               // Условие выхода
+ else return n + SumRec(n - 1);                      // Рекурсивный вызов функции
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine(SumFor(10)); // 55
+
+// Задача 3 найти факториал числа N
+
+// Итеративный подход
+
+int FactorialFor(int n)
+{
+ int result = 1;
+ for (int i = 1; i <= n; i++) result *= i;
+ return result;
+}
+
+// Рекурсия
+
+int FactorialRec(int n)
+{
+ if (n == 1 || n == 0) return 1;                               // Условие выхода
+ else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialFor(10)); // 3628800
+Console.WriteLine(FactorialRec(10)); // 3628800
+
+// Задача 4 вычислать a в натуральной степени n
+// Итеративный и Рекурсивный подходы
+
+int PowerFor(int a, int n)
+{ int result = 1;
+ for (int i = 1; i <= n; i++) result *= a;
+ return result;
+}
+int PowerRec(int a, int n)
+{ //return n == 0 ? 1 : PowerRec(a, n - 1) * a;                // Тернарный способ записи
+ if (n == 0) return 1;                                         // Условие выхода
+ else return PowerRec(a, n - 1) * a;
+}
+
+// Если помнить математику, можно улучшить код:
+
+int PowerRecMath(int a, int n)
+{
+ if (n == 0) return 1;
+ else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+ else return PowerRecMath(a, n - 1) * a;
+}
+
+// Задача 5 в некотором машинном алфавите имеются четыре
+// буквы «а», «и», «с» и «в». Покажите все слова,
+// состоящие из T букв, которые можно построить из букв
+// этого алфавита. Т может быть 1; 2; 3; 4
+
+// Итеративно
+
+char[] s = { 'а', 'и', 'с', 'в' };
+int count = s.Length;
+int n = 1;
+for (int i = 0; i < count; i++)
+{
+    for (int j = 0; j < count; j++)
+    {
+        for (int k = 0; k < count; k++)
+        {
+            for (int l = 0; l < count; l++)
+            {
+                for (int m = 0; m < count; m++)
+                {
+                    Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}{s[m]}");
+                }
+            }
+        }
+    }
+}
+
+// Рекурсия
+
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[5]);
+
+// Задача 5
+//Как посмотреть содержимое папки?
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = @"/Users/sergejkamaneckij/Projects/HelloCode";
+CatalogInfo(path);
+
+// Обход разных структур
+// Решение некоторых задач обход дерева применяется в т.ч. в HTML
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// 0 1 2 3 4 5 6 7 8 9 10 11
+void InOrderTraversal(int pos = 1)
+{
+ if (pos < tree.Length)
+ {
+ int left = 2 * pos;
+ int right = 2 * pos + 1;
+ if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+ Console.WriteLine(tree[pos]);
+ if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+ }
+}
