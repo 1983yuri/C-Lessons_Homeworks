b0808e762a3011a0c43a6f7c4777acc5219121f3
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-26 16:40:14 +0700

    checking 9 seminar

--- Seminars/Seminar9/Program.cs
+++ Seminars/Seminar9/Program.cs
@@ -1,59 +1,59 @@
-﻿int[,] CreateRandom2dArray() // Двумерный массив, сколько запятых столько мер
-{
+﻿// Рекурсия
+int[,] CreateRandom2dArray()
+{   
     Console.Write("Input a number of rows: ");
     int rows = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a number of colums: ");
+    Console.Write("Input a number of columns: ");
     int columns = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a min possible value: ");
+    Console.Write("Input a min possible value ");
     int minValue = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input a max possible value: ");
+    Console.Write("Input a max possible value ");
     int maxValue = Convert.ToInt32(Console.ReadLine());
 
-        int[,] array = new int[rows, columns];
-    
-    for(int i = 0; i < rows; i++) // Пробегаем по всем строкам
-        for(int j = 0; j < columns; j++) // Пробегаем по всем столбцам
-            array[i, j] = new Random().Next(minValue, maxValue + 1);      
-    
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
     return array;
 }
 
-void Show2dArray(int[,] array) // Метод вывода 2х мерного массива
+void Show2dArray(int[,] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++) // Ноль по строкам, 1 по столбцам
+    for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
-           Console.Write(array[i, j] + " ");
-
+            Console.Write(array[i,j] + "\t");
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-// Зачада 1 (Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы)
-
-void Revers2dArray(int[,] array)
+// Задача1: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+/*
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+void Reverse2dArray(int[,] array)
 {
     if(array.GetLength(0) == array.GetLength(1))
     {
-        for(int i = 0; i < array.GetLength(0) - 1; i++) // -1 Потому, что последняя строка нам не нужна, мы берем элементы над гл. диагональю
-           for(int j = i + 1; j < array.GetLength(1); j++)
-           {
-                int temp = array[i, j];
-                array[i, j] = array[j, i];
-                array[j, i] = temp;
-           }
+        for(int i = 0; i < array.GetLength(0) - 1; i++)
+        {
+            for(int j = i + 1; j < array.GetLength(1); j++)
+            {
+                int temp = array[i,j];
+                array[i,j] = array[j,i];
+                array[j,i] = temp;
+            }
+        }
     }
 }
-
-
-int[,] myArray = CreateRandom2dArray();
+Reverse2dArray(myArray);
 Show2dArray(myArray);
-Revers2dArray(myArray);
-Show2dArray(myArray);
+*/
 
-// Задача 2 (Задайте двумерный массив. Напишите программу, которая поменяет местами любые 2 строки массива)
-
+// Задача1: Задайте двумерный массив. Напишите программу, которая поменяет местами любые 2 строки массива.
+/*
 void CreateNewArray(int[,] array, int numA, int numB)
 {
     if( numA != numB && numA >= 0 && numB >= 0 && numA < array.GetLength(0) && numB < array.GetLength(0))
@@ -75,11 +75,11 @@ Console.WriteLine("Input second row");
 int indB = Convert.ToInt32(Console.ReadLine());
 CreateNewArray(myArray, indA, indB);
 Show2dArray(myArray);
+*/
 
 
-
-// Задача 3 (Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент)
-
+// Задача1: Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+/*
 int[] MinElement(int[,] array)
 {
     int[] min = new int[2];
@@ -101,7 +101,7 @@ int[] MinElement(int[,] array)
 int[,] CutArray(int[,] array, int minI, int minJ)
 {
     int[,] cutArray = new int [array.GetLength(0)- 1, array.GetLength(1) - 1];
-    for(int i = 0, x = 0; i < array.GetLength(0); i++, x++)
+    for(int i = 0, x = 0; i < array.GetLength(0);i++, x++)
     {
         if(i != minI)
         {
@@ -123,4 +123,6 @@ Show2dArray(myArray);
 int[] minArray = MinElement(myArray);
 int[,] cutNewArray = CutArray(myArray, minArray[0], minArray[1]);
 Show2dArray(cutNewArray);
+*/
+
 
