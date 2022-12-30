5dce24addf3897876305d3104f597caf4890cb8b
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2022-12-30 11:13:51 +0700

    checking 1 task of 9 homework

--- Homeworks_C#/Homework9/Program.cs
+++ Homeworks_C#/Homework9/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Домашняя работа 9
+
+// Задача 1 (Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии)
+
+void ShowNumbers(int n)
+{
+    if (n > 1)                           // Рекурсивно
+    {
+        Console.Write(n + " ");
+        ShowNumbers(n - 1);
+    }
+    if (n < 1) 
+    {
+        Console.Write(n + " ");
+        ShowNumbers (n + 1);       
+    }
+    if (n == 1)
+    {
+        Console.Write(n + " ");
+    }
+
+/*   
+    if(n > 0)                           // Интеративно
+    {
+        while(n >= 1)
+        {
+            Console.Write(n + " ");
+            n--;
+        }     
+    }
+    else if(n < 0)
+    {
+        while(n <= 1)
+        {
+            Console.Write(n + " ");
+            n++;
+        }
+    }
+*/
+} 
+
+Console.WriteLine("Input the value of the number: ");
+int nums = Convert.ToInt32(Console.ReadLine()); 
+ShowNumbers(nums); 
+
+
+// Задача 2 ()
+
+                         
+
+
